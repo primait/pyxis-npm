e45ad4d015a464df07e96e7e5b4138ffe4ce9a5d
Alessio Santangelo <alessio.santangelo@prima.it>
2019-06-25 14:41:23 +0200

    Buttons update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const devMode =  argv.mode !== 'production'
+    const devMode = argv.mode !== 'production'
     console.log('devMode?', devMode)
     let conf = {
 
@@ -24,11 +24,9 @@ module.exports = (env, argv) => {
                     test: /\.scss$/,
                     include: [path.resolve(__dirname, 'src', 'scss')],
                     use: [
-                        {loader: MiniCssExtractPlugin.loader},
-                        {loader: 'css-loader'},
-                        {
-                            loader: 'postcss-loader',
-                        },
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader' },
+                        { loader: 'postcss-loader' },
                         {
                             loader: 'sass-loader',
                             options: {
@@ -54,19 +52,19 @@ module.exports = (env, argv) => {
                     test: /\.(ttf|eot|svg|woff(2)?|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                     loader: 'file-loader',
                     exclude: /src\/assets\/media/,
-                    options: {name: '[name].[ext]', outputPath: 'assets/fonts/'}
+                    options: { name: '[name].[ext]', outputPath: 'assets/fonts/' }
                 },
                 {
                     test: /\.(jpg|svg|png|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                     loader: 'file-loader',
                     exclude: /src\/assets\/fonts/,
-                    options: {name: '[name].[ext]', outputPath: 'assets/media/'}
+                    options: { name: '[name].[ext]', outputPath: 'assets/media/' }
                 }
             ],
         },
         plugins: [
             new CleanWebpackPlugin(),
-            new MiniCssExtractPlugin({filename: '[name].css'}),
+            new MiniCssExtractPlugin({ filename: '[name].css' }),
         ],
         devServer: {
             contentBase: path.join(__dirname, 'dist'),
@@ -76,7 +74,7 @@ module.exports = (env, argv) => {
     }
 
     if (devMode) {
-        conf = Object.assign(conf, {devtool: 'inline-source-map'})
+        conf = Object.assign(conf, { devtool: 'inline-source-map' })
     }
 
     return conf
